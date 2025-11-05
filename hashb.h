2449e3fa232790344e9d68b2b6c9a7052b8d662c
karthiks9 <karthispeaks@gmail.com>
2025-11-06 00:07:29 +0530

    Add files via upload

---
+++ hashb.h
@@ -0,0 +1,30 @@
+
+#define MAX_HASH_VALUE (90.0)
+#define MIN_HASH_VALUE (-0.5)
+
+#define INITIAL_HASH_VALUE_YES (0.09) //used to be 0.11
+#define INITIAL_HASH_VALUE_NO  (-0.05) //used to be -0.07
+
+#define HASH_VALUE_ADD (0.29) //used to be 0.09
+#define HASH_VALUE_DEC (0.23) //used to be 0.05
+
+#define MAX_HASH_VALUE_V1S (200)
+#define MIN_HASH_VALUE_V1S (-0.90) //-0.60
+
+#define HASH_VALUE_ADD_V1S (0.35) //0.35
+#define HASH_VALUE_DEC_V1S (0.30) //0.30
+
+#if 0
+
+//known good values
+
+#define MAX_HASH_VALUE (90.0)
+#define MIN_HASH_VALUE (-0.5)
+
+#define INITIAL_HASH_VALUE_YES (0.25)
+#define INITIAL_HASH_VALUE_NO  (-0.10)
+
+#define HASH_VALUE_ADD (0.40)
+#define HASH_VALUE_DEC (0.20)
+
+#endif
